bcee5fab6ae7e86a3ac9bbe544514102fce3f067
sofiane bahri <bahrisofiane79@gmail.com>
2023-03-04 20:24:45 +0100

    1 ere partie panier

--- front/js/product.js
+++ front/js/product.js
@@ -11,9 +11,9 @@ fetch(`http://localhost:3000/api/products/${productsId}`)
     
 
     });
+    
+    
     */
-    
-    
 
 
 
@@ -24,31 +24,43 @@ const url = `http://localhost:3000/api/products/${KanapId}`
 
 const getKanap = () => {
     fetch(url)
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(data){
-        const title = (document.getElementById("title").textContent = data.name)
-        const price = (document.getElementById("price").textContent = data.price)
-        const description = (document.getElementById("description").textContent = data.description)
-        //ici on crée l image
-        const img = document.querySelector(".item__img");
-        const createImage = document.createElement('img');
-		createImage.src = data.imageUrl;
-		createImage.alt = data.altTxt;
-        img.appendChild(createImage);
-        
-        //ici on crée la boucle pour les couleurs 
-      for(let couleur of data.colors){
-         const paint = document.getElementById("colors");
-          const elementCouleur = document.createElement("option");
-          paint.appendChild(elementCouleur);
-          elementCouleur.textContent = couleur;
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            const title = (document.getElementById("title").textContent = data.name)
+            const price = (document.getElementById("price").textContent = data.price)
+            const description = (document.getElementById("description").textContent = data.description)
+            //ici on crée l image
+            const img = document.querySelector(".item__img");
+            const createImage = document.createElement('img');
+            createImage.src = data.imageUrl;
+            createImage.alt = data.altTxt;
+            img.appendChild(createImage);
 
+            //ici on crée la boucle pour les couleurs 
+            for (let couleur of data.colors) {
+                const paint = document.getElementById("colors");
+                const elementCouleur = document.createElement("option");
+                paint.appendChild(elementCouleur);
+                elementCouleur.textContent = couleur;
 
-
+            }
+        })
 
 }
-    })
-}
+
+//ici on crée notre panier 
+const addToCart = document.getElementById("addToCart")
+addToCart.addEventListener("click", () =>{
+    const addKanap ={
+        quantity: document.getElementById("quantity").value,
+        couleur: document.getElementById("colors").value,
+        id: KanapId
+
+    }
+
+})
+
+
 getKanap()
