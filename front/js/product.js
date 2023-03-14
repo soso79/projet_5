af1145cc3697944f1e6f385f1689738c145bf87d
sofiane bahri <bahrisofiane79@gmail.com>
2023-03-14 12:11:43 +0100

    ajout en commentaire ELStorage

--- front/js/product.js
+++ front/js/product.js
@@ -88,10 +88,24 @@ addToCart.addEventListener("click", () => {
 
 
 
+/*
+const lStorage = JSON.parse(localStorage.getItem("addToCart"));
+    addKanapLocalStorage = []
+    console.log(lStorage)
+    if (lStorage === null){
+        alert("lsvide")
+        addKanapLocalStorage.push(addToCart)
+        window.localStorage.setItem("addToCart" , JSON.stringify(addKanapLocalStorage))
+        
+    //sinon on push les kanap 
+    }else{
+        alert("ls rempli")
+        addKanapLocalStorage.push(addKanap)
+        window.localStorage.setItem("addToCart", JSON.stringify(addKanapLocalStorage))
+    
+    }
 
-
-
-
+*/
 
 
 
