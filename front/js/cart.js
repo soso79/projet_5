8aecd201ea9a9e0c3e5ba3551ef2f40dd6a7b395
sofiane bahri <bahrisofiane79@gmail.com>
2023-03-27 15:21:44 +0200

    code shaker

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,7 +1,162 @@
 
+const cartTotal = document.querySelector('.cart-total');
+
+// Retrieve cart items from local storage
+const cartItems = JSON.parse(localStorage.getItem("panier"));
+
+function fetchPanier() {
+cartItems.map(product => {
+return fetch(`http://localhost:3000/api/products/${product.id}`)
+.then((response) => response.json())
+.then((data) => ShowPanier(data, product))
+});
+}
+
+
+// Check if cart items exist in local storage
+
+
+function ShowPanier(data, product) {
+if (cartItems && cartItems.length > 0) {
+  // Loop through each cart item and display it on the page
+  
+    // Create a new cart item element
+    const cartItem = document.createElement("article");
+    cartItem.classList.add("cart__item");
+    cartItem.setAttribute("data-id", product.id);
+    cartItem.setAttribute("data-color", product.couleur);
+    
+    // Add cart item HTML content
+    cartItem.innerHTML = `
+      <div class="cart__item__img">
+        <img src="${data.imageUrl}" alt="${data.name}">
+      </div>
+      <div class="cart__item__content">
+        <div class="cart__item__content__description">
+          <h2>${data.name}</h2>
+          <p>${product.couleur}</p>
+          <p>${data.price} €</p>
+        </div>
+        <div class="cart__item__content__settings">
+          <div class="cart__item__content__settings__quantity">
+            <p>Qté : </p>
+            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
+          </div>
+          <div class="cart__item__content__settings__delete">
+            <p class="deleteItem">Supprimer</p>
+          </div>
+        </div>
+      </div>
+    `;
+
+    // Add cart item to the cart section
+    const cartItemsSection = document.getElementById("cart__items");
+    cartItemsSection.appendChild(cartItem);
+};
+}
+fetchPanier();
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 
 const cartTotal = document.querySelector('.cart-total');
 
@@ -60,7 +215,7 @@ if (cartItems && cartItems.length > 0) {
   });
 };
 
-
+*/
 
 
 
